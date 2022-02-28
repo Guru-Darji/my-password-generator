03a6b58035d20cb42b25e0ff2a9e0bd811948707
Guru Darji <gurudarji@ymail.com>
2022-02-28 18:09:15 -0500

    Made the 4 arrays

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 // Assignment Code
+
+
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  var count = Number(prompt("Please enter the amount of characters you would like in your password."));
+
+  var smolLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var bigLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var special = ["|", "~", "!", "@", "$", "%", "&", "?"];
+  var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var genPassword = "";
+
+  for (var i = 0; i < count; i++) {
+    var random = Math.floor(Math.random() * letters.length);
+    genPassword += letters[random];
+  }
+
+  console.log(genPassword);
+
+  return genPassword;
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
