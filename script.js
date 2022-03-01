02fda5dbfde2d7677d0d34a76dc6de85177265c9
Guru Darji <gurudarji@ymail.com>
2022-02-28 20:00:32 -0500

    for loop to create final pass word and put it in a sperate string and returning that string

--- script.js
+++ script.js
@@ -2,25 +2,27 @@
 
 
 var generateBtn = document.querySelector("#generate");
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 
 
 
-  var smolLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  var bigLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  var special = ["|", "~", "!", "@", "$", "%", "&", "?"];
-  var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  
-  var count = "";
-  var checksmolLetters;
-  var checkbigLetters;
-  var checkspecial;
-  var checknumbers;
-  
-  function generatePassword() {
-    var count = (prompt("Please enter the amount of characters you would like in your password."));
+var smolLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var bigLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var special = ["|", "~", "!", "@", "$", "%", "&", "?"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-  while(count <= 8 || count >= 128){
+var count = "";
+var checksmolLetters;
+var checkbigLetters;
+var checkspecial;
+var checknumbers;
+
+function generatePassword() {
+  var count = (prompt("Please enter the amount of characters you would like in your password."));
+
+  while (count <= 8 || count >= 128) {
     alert("That is not the correct amount, Please enter 8 - 128 charaters ONLY!")
     var count = (prompt("How characters would you like your password to be? MUST BE BETWEEN 8-128!"))
   }
@@ -30,28 +32,39 @@ var generateBtn = document.querySelector("#generate");
   var checkspecial = confirm("Click OK, if you would like to include Special characters");
   var checknumbers = confirm("Click OK, if you would like to include Numbers");
 
-  
-}
+
+
+
   var masterPassword = []
 
   if (checksmolLetters) {
-    masterPassword = masterPassword.concat(smolLetters)
+  masterPassword = masterPassword.concat(smolLetters)
   }
 
   if (checkbigLetters) {
-    masterPassword = masterPassword.concat(bigLetters)
+  masterPassword = masterPassword.concat(bigLetters)
   }
-    
+
   if (checkspecial) {
-    masterPassword = masterPassword.concat(special)
+  masterPassword = masterPassword.concat(special)
   }
 
   if (checknumbers) {
-    masterPassword = masterPassword.concat(numbers)
+  masterPassword = masterPassword.concat(numbers)
   }
 
 
-  console.log(masterPassword)
+    console.log(masterPassword)
+
+    var userPassword = ""
+
+    for (var i = 0; i < count; i++) {
+    userPassword = userPassword + masterPassword[Math.floor(Math.random() * masterPassword.length)];
+    console.log(masterPassword)
+    
+  }
+  return userPassword;
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -62,5 +75,3 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
